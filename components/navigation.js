25abd528739271c0a9add94a5f0d93ad0ef8ce15
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-06-06 09:13:42 -0700

    moved index out of folder

--- components/navigation.js
+++ components/navigation.js
@@ -3,7 +3,7 @@ const template = document.createElement('NavBar');
 template.innerHTML = `
 <link rel="stylesheet" href="globals.css">
     <div class ="dive sticky">
-        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/index.html" class="button">Home</a>
+        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="index.html" class="button">Home</a>
         <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/portfolio.html" class="button">Portfolio</a>
         <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/aboutme.html" class="button">About Me</a>
     </div>
