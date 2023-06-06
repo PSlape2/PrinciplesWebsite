eded40ce7cac2b4a998c424b5bd9603cd1e4b9a1
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-06-06 09:10:27 -0700

    redesign navbar, organized folders

--- components/navigation.js
+++ components/navigation.js
@@ -1,11 +1,23 @@
-const template = document.createElement('NavBar');
 
+const template = document.createElement('NavBar');
 template.innerHTML = `
-<div class ="dive sticky">
-    <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="index.html" class="button">Home</a>
-    <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="portfolio.html" class="button">Portfolio</a>
-    <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="aboutme.html" class="button">About Me</a>
-</div>
+<link rel="stylesheet" href="globals.css">
+    <div class ="dive sticky">
+        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/index.html" class="button">Home</a>
+        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/portfolio.html" class="button">Portfolio</a>
+        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/aboutme.html" class="button">About Me</a>
+    </div>
 `;
 
-document.body.appendChild(template.content);
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+    }
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'closed' });
+        shadow.appendChild(template.content);
+    }
+}
+
+customElements.define('NavBar', NavBar);
