d192becb8d1349b938db4b63517a7d8be69be213
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-06-06 09:45:12 -0700

    fix path

--- components/navigation.js
+++ components/navigation.js
@@ -1,11 +1,11 @@
 
 const navTemplate = document.createElement('template');
 navTemplate.innerHTML = `
-    <link rel="stylesheet" href="components/style/globals.css">
+    <link rel="stylesheet" href="/components/style/globals.css">
     <div class ="dive sticky">
-        <a href="index.html" class="button">Home</a>
-        <a href="portfolio.html" class="button">Portfolio</a>
-        <a href="aboutme.html" class="button">About Me</a>
+        <a href="/index.html" class="button">Home</a>
+        <a href="/portfolio.html" class="button">Portfolio</a>
+        <a href="/aboutme.html" class="button">About Me</a>
     </div>
 `;
 
