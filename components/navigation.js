c03285f48756e16431c344cb0d12122b36b6ebf0
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-06-06 09:25:06 -0700

    removed onmouseout code

--- components/navigation.js
+++ components/navigation.js
@@ -3,9 +3,9 @@ const template = document.createElement('NavBar');
 template.innerHTML = `
     <link rel="stylesheet" href="components/style/globals.css">
     <div class ="dive sticky">
-        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="index.html" class="button">Home</a>
-        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/portfolio.html" class="button">Portfolio</a>
-        <a onmouseover="colorChange(this)" onmouseout="colorOut(this)" href="pages/aboutme.html" class="button">About Me</a>
+        <a href="index.html" class="button">Home</a>
+        <a href="pages/portfolio.html" class="button">Portfolio</a>
+        <a href="pages/aboutme.html" class="button">About Me</a>
     </div>
 `;
 
