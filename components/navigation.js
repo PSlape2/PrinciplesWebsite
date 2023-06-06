8a6d1b1f778b8ca6ef27ea73c7471f720ec67a78
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-06-06 10:43:42 -0700

    moved css to file

--- components/navigation.js
+++ components/navigation.js
@@ -2,7 +2,31 @@
 const navTemplate = document.createElement('template');
 
 navTemplate.innerHTML = `
-    <link rel="stylesheet" href="/components/style/globals.css">
+    <style>
+        .button {
+            background-color: #ffffff;
+            color: rgb(0, 0, 0);
+            padding: 20px 25px;
+            text-align: center;
+            display: inline-block;
+            font-size: 12px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+        .button:hover {
+            background-color: gray;
+        }
+        .dive {
+            color: black;
+            background-color: rgb(111, 111, 245);
+            text-align: center;
+            padding: 20px, 20px;
+        }
+        .sticky {
+            position: sticky;
+            top: 0;
+        }
+    </style>
     <div class ="dive sticky">
         <a href="/PrinciplesWebsite2022/index.html" class="button">Home</a>
         <a href="/PrinciplesWebsite2022/portfolio.html" class="button">Portfolio</a>
