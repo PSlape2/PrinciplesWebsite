c22766db90da8b7d2ccf1c41b7040d4bd3753ab6
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-06-06 10:38:54 -0700

    fix pathing

--- components/navigation.js
+++ components/navigation.js
@@ -4,9 +4,9 @@ const navTemplate = document.createElement('template');
 navTemplate.innerHTML = `
     <link rel="stylesheet" href="/components/style/globals.css">
     <div class ="dive sticky">
-        <a href="/index.html" class="button">Home</a>
-        <a href="/portfolio.html" class="button">Portfolio</a>
-        <a href="/aboutme.html" class="button">About Me</a>
+        <a href="/PrinciplesWebsite2022/index.html" class="button">Home</a>
+        <a href="/PrinciplesWebsite2022/portfolio.html" class="button">Portfolio</a>
+        <a href="/PrinciplesWebsite2022/aboutme.html" class="button">About Me</a>
     </div>
 `;
 
